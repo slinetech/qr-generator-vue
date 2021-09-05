0d1186e7c69278cbf74ad2b1666364025855717e
Xavier Rayas <xavirayas@gmail.com>
2021-09-05 14:31:57 -0600

    Create Componentes to generate QR Code

---
+++ src/components/qr/Generator.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="container">
+        formulario para la generacion
+        <QR />
+    </div>
+</template>
+
+<script>
+import QR from './QR.vue'
+
+export default {
+    name: 'Generator',
+    components: {
+        QR
+    }
+}
+</script>
+<style scoped>
+
+</style>
---
+++ src/components/qr/QR.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="container">
+        <vue-qr text="Hello world!" :callback="test" qid="testid"></vue-qr>
+    </div>
+</template>
+
+<script>
+import VueQr from 'vue-qr'
+export default {
+    name: 'QR',
+    components: {
+        VueQr
+    },
+    methods: {
+        test(dataUrl,id){
+            console.log(dataUrl, id);
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
