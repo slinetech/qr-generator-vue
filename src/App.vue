826855d38a4cad8e4d67d7df2ada411cb95210d8
Xavier Rayas <xavirayas@gmail.com>
2021-09-08 11:59:46 -0600

    modified text to NAV

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
     <nav class="navbar navbar-light bg-light">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="#">Generator QR Code</a>
+      <div class="container-fluid t-c">
+        <a class="navbar-brand" href="/">Generator QR Code</a>
       </div>
     </nav>
     <Generator />
@@ -21,5 +21,14 @@ export default {
 </script>
 
 <style>
-
+  .t-c{
+    display: inline-block !important;
+    text-align: center;
+  }
+  /*Small devices (landscape phones, 576px and up)*/
+    @media (min-width: 576px) {
+        .t-c{
+          display: flex !important;
+        }
+    }
 </style>
